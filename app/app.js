2bcff38ecea707c2062d0b423904e40018c73d51
Gonzalo Díaz Ailán <Devlain@Devlain.local>
2017-08-26 12:31:56 -0300

    feat: added files for game

--- app/app.js
+++ app/app.js
@@ -3,9 +3,45 @@
 	angular.module("gameApp", [])
 
 	.controller("mainCtrl", mainCtrl);
-	mainCtrl.$injector = [$scope];
+	mainCtrl.$inject = ['$scope'];
 
 	function mainCtrl($scope) {
+
+		const monsterBook = {
+			plant: {
+				name: 'plant',
+				life: 5,
+				pow: 1,
+				int: 4,
+				ski: 3,
+				spe: 2,
+				def: 1,
+				skills: [
+					{
+						name: "Zap",
+						dmg: 1,
+						acc: 3,
+
+					},{
+
+					},{
+
+					},{
+
+					},{
+
+					}
+				]
+			},
+			golem: {
+
+			},
+			tiger: {
+
+			}
+		}
+
+		$scope.monsterBook = monsterBook;
 		
 	}
 })();
